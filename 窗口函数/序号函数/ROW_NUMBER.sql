594849e7bd2d16daff11c00d292a3ab4b8bfe416
天明 <1036498144@qq.com>
2024-12-30 18:43:24 +0800

    添加员工表及相关数据插入，创建基于薪资的行号查询

---
+++ 窗口函数/序号函数/ROW_NUMBER.sql
@@ -0,0 +1,27 @@
+-- 从employee表中选择部门名称(dname)、员工姓名(ename)、薪水(salary)以及一个基于部门和薪水排序的行号(rk) --
+SELECT dname, ename, salary, ROW_NUMBER() OVER (
+        PARTITION BY
+            dname
+        ORDER BY salary DESC
+    ) AS `rk`
+FROM employee;
+
+SELECT dname, ename, eid, salary, RANK() OVER (
+        PARTITION BY
+            dname
+        ORDER BY salary DESC
+    ) AS `rk`
+FROM employee;
+
+SELECT dname, ename, eid, salary, DENSE_RANK() OVER (
+        PARTITION BY
+            dname
+        ORDER BY salary DESC
+    ) AS `rk`
+FROM employee
+
+-- 查询employee表中的部门名称(dname)、员工姓名(ename)、员工编号(eid)、薪水(salary)以及一个基于薪水排序的行号(rk) --
+SELECT dname, ename, eid, salary, DENSE_RANK() OVER (
+        ORDER BY salary DESC
+    ) AS `rk`
+from employee;
